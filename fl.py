593a2ac445737b978308f2b193163e55b915e8fc
Voximir <pek795b@gmail.com>
2026-01-11 23:17:42 +0700

    aw4ts

--- fl.py
+++ fl.py
@@ -1,9 +1,10 @@
 from flask import Flask, send_file
 
 app = Flask(__name__)
-@app.route("/tiktokZyNtN47qQxUls7qm50zRZKugGykimdtK.txt", methods=["GET"])
-def verify():
-    return send_file("tiktokZyNtN47qQxUls7qm50zRZKugGykimdtK.txt")
+# send file as requested
+@app.route("/<path:filename>")
+def serve_file(filename):
+    return send_file(filename)
 
 if __name__ == "__main__":
     app.run(host="172.31.24.141", port=80)
