4b98cea89499df93524d39c84b35933c1136376b
Voximir <pek795b@gmail.com>
2026-01-11 23:15:02 +0700

    wtwr

--- fl.py
+++ fl.py
@@ -1,9 +1,9 @@
 from flask import Flask, send_file
 
 app = Flask(__name__)
-@app.route("/verify/tiktok0h2WcHjMBYRbT1AukihT6pD2JvElI19D.txt", methods=["GET"])
+@app.route("/tiktokZyNtN47qQxUls7qm50zRZKugGykimdtK.txt", methods=["GET"])
 def verify():
-    return send_file("tiktok0h2WcHjMBYRbT1AukihT6pD2JvElI19D.txt")
+    return send_file("tiktokZyNtN47qQxUls7qm50zRZKugGykimdtK.txt")
 
 if __name__ == "__main__":
     app.run(host="172.31.24.141", port=80)
